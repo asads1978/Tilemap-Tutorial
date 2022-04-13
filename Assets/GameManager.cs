ed682d834e01f7863e9e9115cabc83095c63abe3
asads1978 <andrew.sadler@yahoo.co.uk>
2022-04-13 23:55:20 +0100

    Tilemap Tutorial video 3

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,20 @@ public class GameManager : MonoBehaviour
     [SerializeField] Tilemap tilemap;
     [SerializeField] Tile pathTile;
     [SerializeField] Tile blockerTile;
+
+    [SerializeField] int height = 32;
+    [SerializeField] int width = 32;
+
+    int[,] mapData;
+
+    void Start() {
+       mapData = new int[width,height];
+
+       GenerateMap();
+       ClearTilemap();
+       DrawMap();
+
+    }
 
     // Update is called once per frame
     void Update()
@@ -28,5 +43,35 @@ public class GameManager : MonoBehaviour
         Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         return tilemap.WorldToCell(mouseWorldPosition);
+    }
+
+    private void GenerateMap()
+    {
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+            {
+                mapData[x,y] = 0;
+            }
+    }
+
+    private void DrawMap()
+    {
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+            {
+                if (mapData[x,y] == 0)
+                {
+                    tilemap.SetTile(new Vector3Int(x,y,0), pathTile);
+                }
+                else if (mapData[x,y] == 1)
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), blockerTile);
+                }    
+            }
+    }
+
+    private void ClearTilemap()
+    {
+        tilemap.ClearAllTiles();
     }
 }
